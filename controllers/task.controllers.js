c3212537675e025dc2066486e5e3120c6a4698d2
Akshat Jain <aj1508109@gmail.com>
2025-08-29 18:34:19 +0530

    querying sorting filtering

--- controllers/task.controllers.js
+++ controllers/task.controllers.js
@@ -20,10 +20,41 @@ exports.createtask = async (req, res, next) => {
 
 exports.getTasks = async (req, res) => {
     try{
-        const tasks = await Task.find({user: req.user._id}).sort({createdAt: -1})
-        res.status(200).json({
+        const { status, priority, sortBy, order = "asc", page=1, limit = 10, search}=req.query
+
+        const pageNum = Number(page) || 1;
+        const limitNum = Number(limit) || 10
+
+        let filter = {user: req.user._id}
+        if(status) filter.status = status;
+        if(priority) filter.priority=priority
+
+        if(search){
+            filter.$or = [
+                {title: {$regex: search,$options:"i"}},
+                {description: {$regex: search, $options: "i"}},
+            ]
+        }
+
+        let sortOptions={}
+        if(sortBy) {
+            sortOptions[sortBy]=order==="desc" ? -1 : 1
+        }else{
+            sortOptions = { createdAt: -1 }
+        }
+        const tasks = await Task.find(filter)
+            .sort(sortOptions)
+            .skip((page-1)*limit)
+            .limit(Number(limit))
+
+        const total = await Task.countDocuments(filter)
+
+        res.json({
             success: true,
-            tasks
+            total,
+            page: Number(page),
+            pages: Math.ceil(total/limit),
+            tasks,
         })
     }catch(error){
         next(error)
